2515b810f7e101e918fb0dc9f85870fae5c2190e
alexcoder04 <alexcoder04@protonmail.com>
2022-01-21 15:33:31 +0100

    error handling and stuff

--- server.py
+++ server.py
@@ -11,11 +11,12 @@
 # requires: flask, werkzeug, argparse
 # 
 
-import time
-import os
 from flask import Flask, render_template, send_file, request, redirect
 from werkzeug.utils import secure_filename
 import argparse
+import os
+import sys
+import time
 
 app = Flask(__name__)
 
@@ -53,6 +54,8 @@ def upload():
         if f.filename is None:
             continue
         fname = secure_filename(f.filename)
+        if fname == "":
+            return redirect("/error/No file selected".replace(" ", "%20"))
         f.save(os.path.join(app.config["SHARE_DIR"], fname))
     return redirect(f"/?f={[i.filename for i in files]}")
 
@@ -60,11 +63,18 @@ def upload():
 @app.route("/dl/<path:file_path>")
 def download(file_path):
     requested_file = os.path.abspath(os.path.join(app.config["SHARE_DIR"], file_path))
+    if os.path.getmtime(requested_file) + app.config["KEEP"] < time.time():
+        os.remove(requested_file)
+        return redirect("/error/This file was deleted".replace(" ", "%20"))
     if not requested_file.startswith(app.config["SHARE_DIR"]):
         return "ERROR"
     if not os.path.isfile(requested_file):
         return "ERROR"
     return send_file(requested_file)
+
+@app.route("/error/<message>")
+def error(message):
+    return render_template("error.html", message=message)
 
 # ifmain
 if __name__ == "__main__":
@@ -73,14 +83,22 @@ if __name__ == "__main__":
             )
 
     parser.add_argument("--debug", action="store_true", help="run flask in debug mode")
-    parser.add_argument("--share-dir", default="/media/share", type=str, help="folder to share")
-    parser.add_argument("--keep", default=(60*5), type=int, help="deleted files after so many seconds")
+    parser.add_argument("--keep", default=(60*5), type=int, help="delete files after so many seconds")
     parser.add_argument("--port", default=8899, type=int, help="port to listen on")
+    parser.add_argument("--share-dir", default="/tmp/sharebox", type=str, help="folder to share")
 
     args = parser.parse_args()
 
     app.config["SHARE_DIR"] = args.share_dir
     app.config["KEEP"] = args.keep
+
+    if not os.path.isdir(app.config["SHARE_DIR"]):
+        try:
+            print("Warning: shared folder does not exist, creating...")
+            os.mkdir(app.config["SHARE_DIR"])
+        except Exception:
+            print("Fatal error: shared folder could not be created")
+            sys.exit(1)
 
     app.run(port=args.port, debug=args.debug, host="0.0.0.0")
 
---
+++ static/confirm.js
@@ -0,0 +1,8 @@
+function confirmForm() {
+  const file = document.getElementById("upload").value;
+  if (file == undefined || file == null || file == "") {
+    window.alert("You need to select a file!!!");
+    return false;
+  }
+  return true;
+}
